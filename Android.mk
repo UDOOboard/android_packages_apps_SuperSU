4698f104e5a6a93458968a38912ad9518ffeccac
Francesco Montefoschi <francesco.monte@gmail.com>
2016-04-29 12:58:25 +0200

    Renamed Superuser in SuperSU

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := SuperSU
-LOCAL_SRC_FILES := Superuser.apk
+LOCAL_SRC_FILES := SuperSU.apk
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
